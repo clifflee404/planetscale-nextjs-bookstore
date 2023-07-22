45428bde85821ecda3978ff8dd2688b013071253
cliff <clifflee1022@gmail.com>
2023-07-22 18:58:09 +0800

    feat: api addBook & readbooks ok

---
+++ app/api/book/route.ts
@@ -0,0 +1,50 @@
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+const prisma = new PrismaClient()
+
+export const GET = async (req:NextRequest, res: NextResponse) => {
+  // return new Response(JSON.stringify({data: []}), { status: 200})
+  return await readBooks()
+}
+
+export const POST = async (req: NextRequest, res: NextResponse) => {
+  // const reqBody = await req.json()
+  // console.log('---post body:', reqBody);
+
+  // return new Response(JSON.stringify({}), { status: 200 })
+  return await addBook(req, res)
+}
+
+// 查询书籍
+async function readBooks() {
+  try {
+    const books = await prisma.book.findMany()
+    return new Response(JSON.stringify({data: books, code: 0}), { status: 200})
+  } catch (error) {
+    console.error(error)
+    return new Response(JSON.stringify({error:  'Error reading frome database', code: 1}), { status: 500})
+  }
+}
+
+// 添加书籍
+async function addBook(req:NextRequest , res: NextResponse){
+  const reqBody = await req.json()
+  console.log('---post body:', reqBody);
+  try {
+    const newEntry = await prisma.book.create({
+      data: {
+        bookTitle: reqBody.title,
+        bookAuthor: reqBody.author,
+        bookTag: reqBody.tag,
+        bookDescription: reqBody.description,
+      }
+    })
+    return new Response(JSON.stringify({success: true, data: newEntry}), { status: 200 })
+  } catch (error) {
+    console.error("Request error", error)
+    return new Response(JSON.stringify({
+      error: 'Error adding book'
+    }), { status: 500 })
+  }
+}
--- app/create/page.tsx
+++ app/create/page.tsx
@@ -76,15 +76,15 @@ function AddBook() {
             </div>
           </div>
           <div className="sm:col-span-2">
-            <label htmlFor="tags" className="block text-sm font-semibold leading-6 text-gray-900">
+            <label htmlFor="tag" className="block text-sm font-semibold leading-6 text-gray-900">
               类别
             </label>
             <div className="mt-2.5">
               <input
                 type="text"
-                name="tags"
-                id="tags"
-                autoComplete="tags"
+                name="tag"
+                id="tag"
+                autoComplete="tag"
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
